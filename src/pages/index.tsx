88575d9785b43da5c2f30accc5a7b3c27e5d0e89
AldrinEstrada <aldrinestradabcit@gmail.com>
2024-02-12 22:40:14 -0800

    midterm-adjustments2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ const Home = () => {
           src={'/images/brutherlogo.png'}
           height={120}
           width={100}
-          alt={'logo'}
+          alt="bruther logo"
         />
 
         <h1>Bruhther</h1>
